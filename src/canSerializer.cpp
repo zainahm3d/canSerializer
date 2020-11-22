0939e877b3e272c38a258d2acd84d0dcd5367c02
Zain Ahmed <zainahmed1559@gmail.com>
2020-11-22 12:11:14 -0500

    initial commit

---
+++ src/canSerializer.cpp
@@ -0,0 +1,46 @@
+/**
+ * @file canSerializer.c
+ * @author Zain Ahmed
+*/
+
+#include "canSerializer.h"
+
+// Calcuates a checksum
+uint8_t calculateChecksum(CANFrame *pFrame)
+{
+    uint8_t computedChecksum = 0;                                           // will hold our checksum as we calculate
+    uint8_t bytesToSum = sizeof(*pFrame) - sizeof(pFrame->checksum);        // do not include the checksum when calculating the checksum
+
+    uint8_t *bytePtr = (uint8_t *)pFrame;                                   // get a pointer to struct so we can access individual bytes
+
+    for (int i = 0; i < bytesToSum; i++) {
+      computedChecksum += *(bytePtr + i);                                   // dereference and sum each byte
+    }
+
+    return computedChecksum;
+}
+
+/**
+ * @param pFrame: Pointer to a CAN frame.
+ * @note: Ensure all other data of frame is filled before calling fillChecksum().
+*/
+void fillChecksum(CANFrame *pFrame)
+{
+    pFrame->checksum = calculateChecksum(pFrame);                           // append the checksum to the packet
+}
+
+/**
+ * @param pFrame: Pointer to a CAN frame.
+ * Returns true if the pFrame struct is valid, false if it is invalid.
+*/
+bool validateChecksum(CANFrame *pFrame)
+{
+    if (pFrame->checksum == calculateChecksum(pFrame))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
---
+++ src/canSerializer.h
@@ -0,0 +1,32 @@
+/**
+ * @file canSerializer.h
+ *
+ * @brief Helper tool for serializing CAN messages into uniformaly sized packets for tranmission over a non CAN bus transport layer.
+ *
+ * @author Zain Ahmed
+*/
+
+#ifndef CANSERIALIZER_H
+#define CANSERIALIZER_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+// a library independant way of inputting and outputting CAN data.
+// input all values other than the checksum.
+// use a pointer to an instance of this struct to send data.
+// struct is packed to ensure no padding bytes are added by GCC.
+typedef struct __attribute__((__packed__))
+{
+  bool extended;            // true if 29 bit ID, false if 11 bit
+  uint32_t id;              // CAN frame ID
+  uint8_t dlc;              // CAN frame data length code (0 - 8).
+  uint8_t data[8];          // CAN frame data array.
+  uint8_t checksum;         // DO NOT EDIT. This is filled by fillChecksum()
+} CANFrame;
+
+// Prototypes
+void fillChecksum(CANFrame *pFrame);
+bool validateChecksum(CANFrame *pFrame);
+
+#endif // CANSERIALIZER_H
